39197465934ab938f2354500fc4f0cb03e902a87
Thierry Zamofing <thierry.zamofing@psi.ch>
2013-07-02 15:45:30 +0200

    wip

--- hdfGrid.py
+++ hdfGrid.py
@@ -4,135 +4,93 @@ import wx.grid
 import numpy as N
 #http://wxpython-users.1045709.n5.nabble.com/filling-a-wxgrid-td2348720.html
 
-
-class Knob:
-    """
-    Knob - simple class with a "setKnob" method.  
-    A Knob instance is attached to a Param instance, e.g. param.attach(knob)
-    Base class is for documentation purposes.
-    """
-    def setKnob(self, value):
-        pass
-
-
-class Param:
-    """
-    The idea of the "Param" class is that some parameter in the GUI may have
-    several knobs that both control it and reflect the parameter's state, e.g.
-    a slider, text, and dragging can all change the value of the frequency in
-    the waveform of this example.  
-    The class allows a cleaner way to update/"feedback" to the other knobs when 
-    one is being changed.  Also, this class handles min/max constraints for all
-    the knobs.
-    Idea - knob list - in "set" method, knob object is passed as well
-      - the other knobs in the knob list have a "set" method which gets
-        called for the others.
-    """
-    def __init__(self, initialValue=None, minimum=0., maximum=1.):
-        self.minimum = minimum
-        self.maximum = maximum
-        if initialValue != self.constrain(initialValue):
-            raise ValueError('illegal initial value')
-        self.value = initialValue
-        self.knobs = []
-        
-    def attach(self, knob):
-        self.knobs += [knob]
-        
-    def set(self, value, knob=None):
-        self.value = value
-        self.value = self.constrain(value)
-        for feedbackKnob in self.knobs:
-            if feedbackKnob != knob:
-                feedbackKnob.setKnob(self.value)
-        return self.value
-
-    def constrain(self, value):
-        if value <= self.minimum:
-            value = self.minimum
-        if value >= self.maximum:
-            value = self.maximum
-        return value
-
-
-class SliderGroup(Knob):
-    def __init__(self, parent, label, param):
+class SliderGroup():
+    def __init__(self, parent, label, range=(0,100),cbFuncData=None):
         self.sliderLabel = wx.StaticText(parent, label=label)
         self.sliderText = wx.TextCtrl(parent, -1, style=wx.TE_PROCESS_ENTER)
         self.slider = wx.Slider(parent, -1)
-        self.slider.SetMax(param.maximum*1000)
-        self.setKnob(param.value)
-        
+        self.slider.SetRange(range[0],range[1])      
         sizer = wx.BoxSizer(wx.HORIZONTAL)
         sizer.Add(self.sliderLabel, 0, wx.EXPAND | wx.ALIGN_CENTER | wx.ALL, border=2)
         sizer.Add(self.sliderText, 0, wx.EXPAND | wx.ALIGN_CENTER | wx.ALL, border=2)
         sizer.Add(self.slider, 1, wx.EXPAND)
         self.sizer = sizer
-
         self.slider.Bind(wx.EVT_SLIDER, self.sliderHandler)
         self.sliderText.Bind(wx.EVT_TEXT_ENTER, self.sliderTextHandler)
-
-        self.param = param
-        self.param.attach(self)
+        self.cbFuncData=cbFuncData
+        self.txtFmt='%d'
+        self.value=None
 
     def sliderHandler(self, evt):
-        value = evt.GetInt() / 1000.
-        self.param.set(value)
+        value = evt.GetInt()
+        self.sliderText.SetValue(self.txtFmt%value)
+        self.value=value
+        if self.cbFuncData:
+          self.cbFuncData[0](0,self.cbFuncData[1])
         
     def sliderTextHandler(self, evt):
-        value = float(self.sliderText.GetValue())
-        self.param.set(value)
+        value = int(self.sliderText.GetValue())
+        self.slider.SetValue(value)
+        value = self.slider.Value
+        self.sliderText.SetValue(self.txtFmt%value)
+        self.value=value
+        if self.cbFuncData:
+          self.cbFuncData[0](1,self.cbFuncData[1])
         
-    def setKnob(self, value):
-        self.sliderText.SetValue('%g'%value)
-        self.slider.SetValue(value*1000)
-
-
-
 class TableBase(wx.grid.PyGridTableBase):
     def __init__(self, data):
         wx.grid.PyGridTableBase.__init__(self)
         self.data = data
+        self.view = data[0,...]
 
     def GetNumberRows(self):
-        return self.data.shape[0]
+        return self.view.shape[0]
 
     def GetNumberCols(self):
-        return self.data.shape[1]
+        return self.view.shape[1]
 
     def GetValue(self, row, col):
-        return self.data[row][col]
+        return self.view[row,col]
 
 class Grid(wx.grid.Grid):
-    def __init__(self, parent, data):
-        wx.grid.Grid.__init__(self, parent, -1)
+  def __init__(self, parent, data):
+    wx.grid.Grid.__init__(self, parent, -1)
 
-        table = TableBase(data)
-        self.SetTable (table, True)
+    table = TableBase(data)
+    self.SetTable (table, True)
+  @staticmethod
+  def OnChangeAxVal(idx,args):
+    tbl=args.GetTable()
+    tbl.view = tbl.data[10,...]
+    pass
 
 class HdfGridFrame(wx.Frame):
-    def __init__(self, parent,lbl,hid):
-        wx.Frame.__init__(self, parent, title='HDFGridView: '+lbl,size=wx.Size(750, 650))
+  def __init__(self, parent,lbl,hid):
+    wx.Frame.__init__(self, parent, title='HDFGridView: '+lbl,size=wx.Size(750, 650))
 
-        pan = wx.Panel(self, -1)
+    pan = wx.Panel(self, -1)
 
-        t=type(hid)
-        if t==h5py.h5d.DatasetID:
-          obj=h5py.Dataset(hid)
-        #grid = Grid(pan, N.random.rand(3000,4000))
-        grid = Grid(pan, obj.value)
+    t=type(hid)
+    if t==h5py.h5d.DatasetID:
+      obj=h5py.Dataset(hid)
+    #grid = Grid(pan, N.random.rand(3000,4000))
+    self.grid = Grid(pan, obj.value)      
+    
+    sizer = wx.BoxSizer(wx.VERTICAL)
+    sizer.Add(self.grid, 1, wx.EXPAND)
+    shape=obj.value.shape
+    idxRowCol=(1,2)
+    ctrlAxis=[]
+    for idx in range(len(shape)-2):
+      wxAxCtrl=SliderGroup(pan, label='Axis:%d'%idx,cbFuncData=(Grid.OnChangeAxVal,(self.grid)))
+      ctrlAxis.append(wxAxCtrl)
+      sizer.Add(wxAxCtrl.sizer, 0, wx.EXPAND | wx.ALIGN_CENTER | wx.ALL, border=5)
 
-        self.f0= Param(2., minimum=0., maximum=6.)
-        self.frequencySliderGroup = SliderGroup(pan, label='Frequency f0:', \
-            param=self.f0)
-        
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(grid, 1, wx.EXPAND)
-        sizer.Add(self.frequencySliderGroup.sizer, 0, \
-            wx.EXPAND | wx.ALIGN_CENTER | wx.ALL, border=5)
-        pan.SetSizer(sizer)
-        pan.Layout()
-        self.Centre()
+    self.ctrlAxis=ctrlAxis
+    pan.SetSizer(sizer)
+    pan.Layout()
+    self.Centre()
+        #self.grid.SetTable(())        
 
 
 if __name__ == '__main__':
--- hdfImage.py
+++ hdfImage.py
@@ -1,11 +1,77 @@
+#!/usr/bin/env python
+"""
+An example of how to use wx or wxagg in an application with the new
+toolbar - comment out the setA_toolbar line for no toolbar
+"""
+
 import os
 import wx,h5py
 
+# Used to guarantee to use at least Wx2.8
+#import wxversion
+#wxversion.ensureMinimal('2.8')
+
+from numpy import arange, sin, pi
+import matplotlib
+#matplotlib.use('WX')
+#from matplotlib.backends.backend_wx import FigureCanvasWx as FigureCanvas
+matplotlib.use('WXAgg')
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.backends.backend_wx import NavigationToolbar2Wx
+from matplotlib.figure import Figure
 
 class HdfImageFrame(wx.Frame):
   def __init__(self, parent,lbl,hid):
     wx.Frame.__init__(self, parent, title=lbl, size=wx.Size(750, 350))
-    #self.wxLstCtrl=HdfAttrListCtrl(self)
-    #self.wxLstCtrl.ShowAttr(hid)  
+
+    self.figure = Figure()
+    self.axes = self.figure.add_subplot(111)
+    t = arange(0.0,3.0,0.01)
+    s = sin(2*pi*t)
+    
+    self.axes.plot(t,s)
+    self.canvas = FigureCanvas(self, -1, self.figure)
+    
+    self.sizer = wx.BoxSizer(wx.VERTICAL)
+    self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
+    self.SetSizer(self.sizer)
+    self.Fit()
+    self.add_toolbar()  # comment this out for no toolbar
+    
     self.Centre()
- 
+
+  def add_toolbar(self):
+    self.toolbar = NavigationToolbar2Wx(self.canvas)
+    self.toolbar.Realize()
+    if wx.Platform == '__WXMAC__':
+      # Mac platform (OSX 10.3, MacPython) does not seem to cope with
+      # having a toolbar in a sizer. This work-around gets the buttons
+      # back, but at the expense of having the toolbar at the top
+      self.SetToolBar(self.toolbar)
+    else:
+      # On Windows platform, default window size is incorrect, so set
+      # toolbar width to figure width.
+      tw, th = self.toolbar.GetSizeTuple()
+      fw, fh = self.canvas.GetSizeTuple()
+      # By adding toolbar in sizer, we are able to put it at the bottom
+      # of the frame - so appearance is closer to GTK version.
+      # As noted above, doesn't work for Mac.
+      self.toolbar.SetSize(wx.Size(fw, th))
+      self.sizer.Add(self.toolbar, 0, wx.LEFT | wx.EXPAND)
+    # update the axes menu on the toolbar
+    self.toolbar.update()
+
+if __name__ == '__main__':
+  class App(wx.App):
+    def OnInit(self):
+      fnHDF='/home/zamofing_t/Documents/prj/libDetXR/python/libDetXR/e14472_00033.hdf5'
+      lbl='mcs'
+      lbl='pilatus_2'
+      fid = h5py.h5f.open(fnHDF)
+      hid = h5py.h5o.open(fid,'/entry/dataScan00033/'+lbl)
+      frame = HdfImageFrame(None,lbl,hid)
+      frame.Show()
+      return True
+  
+  app = App()
+  app.MainLoop()
